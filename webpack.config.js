986ea53fe8ac55c5cd35a843b616e2395b1f8642
alvaro-bm <=>
2023-04-13 14:37:06 -0600

    Added webpackconfig, .babelrc and path (module)

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+module.exports = {
+    entry: path.join(__dirname, "src", "index.jsx"),
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "main.js"
+    },
+    mode: process.env.NODE_ENV || "development",
+    module: {
+        rules: [{
+            test: /\.(js || jsx)$/,
+            exclude: /node_module/,
+            use: {
+                loader: "babel_loader"
+            }
+        }]
+    },
+    resolve: {
+        extensions: [".js", ".json", ".jsx"],
+        modules: [
+            path.resolve(__dirname, "src"),
+            "node_modules"
+        ]
+    }
+}
