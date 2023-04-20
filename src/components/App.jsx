1138f74d16dc854ee48816141e3ee60d0e69b969
alvaro-bm <=>
2023-04-20 15:00:38 -0600

    adding theme functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,6 +9,8 @@ import Menu from "./Menu";
 import Projects from "./Projects";
 import Education from "./Education";
 import Books from "./Books";
+import Currently from "./Currently";
+import Footer from "./Footer";
 
 const App = () => {
     const [theme, setTheme] = useState("light");
@@ -16,8 +18,11 @@ const App = () => {
 
     const toogleTheme = () => {
         const toogled = theme === "light" ? "dark" : "light";
+        const mainContainer = document.querySelector("#app");
         setTheme(toogled);
         window.localStorage.setItem("theme", toogled);
+        mainContainer.style.backgroundColor = toogled === "light" ? "#f8f8f8" : "black";
+        mainContainer.style.color = toogled === "light" ? "white" : "black";
     }
 
     const toogleLang = () => {
@@ -27,15 +32,26 @@ const App = () => {
     }
 
     useEffect(() => {
+        const mainContainer = document.querySelector("#app");
         const currentTheme = window.localStorage.getItem("theme");
-        (currentTheme) ? setTheme(currentTheme) : setTheme("dark");
+        //(currentTheme) ? setTheme(currentTheme) : setTheme("dark");
+        if (currentTheme) {
+            setTheme(currentTheme);
+            mainContainer.style.backgroundColor = currentTheme === "light" ? "#f8f8f8" : "black";
+            mainContainer.style.color = currentTheme === "light" ? "black" : "white";
+        }
+        else {
+            setTheme("dark");
+            mainContainer.style.backgroundColor = "black";
+            mainContainer.style.color = "white";
+        }
 
         const langPage = window.localStorage.getItem("lang");
         (langPage) ? setLang(langPage) : setLang("es");
     }, [theme, lang]);
 
     const ButtonToogleTheme = () => <button onClick={toogleTheme}>{theme === 'light' ? '🌞' : '🌚'}</button>;
-    const ButtonToogleLang = () => <button onClick={toogleLang}>{lang === 'es' ? '🇲🇽' : '🇱🇷'}</button>;
+    const ButtonToogleLang = () => <button onClick={toogleLang}>{lang === 'es' ? '🇲🇽' : '🇺🇸'}</button>;
     return (
         <>
             <Header
@@ -45,13 +61,15 @@ const App = () => {
                 buttonTheme={<ButtonToogleTheme />} />
             <div className="app-container-content">
                 <Introduce lang={lang} />
-                <Description lang={lang} />
-                <Skills lang={lang} />
+                <Description lang={lang} theme={theme} />
+                <Skills lang={lang} theme={theme} />
                 <Menu lang={lang} />
                 <Projects lang={lang} />
                 <Education lang={lang} />
-                <Books lang={lang} />
+                <Books lang={lang} theme={theme} />
+                <Currently lang={lang} />
             </div>
+            <Footer lang={lang} theme={theme} />
         </>
     );
 }
