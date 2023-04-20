a48023d17d4039c87255ab3fd411fd2bc3da7bc7
alvaro-bm <=>
2023-04-20 11:47:50 -0600

    add more space to the height

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,14 +4,14 @@ import "./Menu.scss";
 const Menu = ({ lang }) => {
     return (
         <>
+            <h2>{lang === "es" ? "He hecho..." : "I have done..."}</h2>
             <section className="menu-works">
-                <h2>{lang === "es" ? "He hecho..." : "I have done..."}</h2>
                 <div className="options-menu">
                     <a href="#projects">📁 {lang === "es" ? "Proyectos" : "Projects"}</a>
                     <a href="#education">📒 {lang === "es" ? "Educación" : "Education"}</a>
                     <a href="#books">🔖 {lang === "es" ? "Libros (leídos)" : "Books (read)"}</a>
                     <a href="#now">📌 {lang === "es" ? "Actualmente" : "Now"}</a>
-                </div> 
+                </div>
             </section>
         </>
     );
