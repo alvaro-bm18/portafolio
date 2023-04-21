3851179f9723059e449b27eaa955dc6659ad0d07
alvaro-bm <=>
2023-04-20 22:54:48 -0600

    changed 'box-shadow' to 'boxShadow' 'cause react

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,14 @@ import "./Header.scss";
 
 const Header = ({ theme, bg, buttonLang, buttonTheme }) => {
     return (
-        <header id="top" style={{ 'background-image': `url(${bg})` }}>
+        <header id="top" style={{ 'backgroundImage': `url(${bg})` }}>
             <div className="app-options">
                 {buttonTheme}
                 {buttonLang}
             </div>
             <div
                 className="photo-perfil"
-                style={{ 'box-shadow': `inset 0 -70px 0 0 ${theme === 'light' ? '#f8f8f8' : 'black'}` }}>
+                style={{ 'boxShadow': `inset 0 -70px 0 0 ${theme === 'light' ? '#f8f8f8' : 'black'}` }}>
                 <img src="https://avatars.githubusercontent.com/u/114254709" alt="perfil" />
             </div>
         </header>
