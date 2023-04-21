64f2ffaf8021ce542471c45e51278a190516a482
alvaro-bm <=>
2023-04-20 22:55:42 -0600

    Removed the options of menu and pased them as props to use in app.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 import "./Menu.scss";
 
-const Menu = ({ lang }) => {
+const Menu = ({ lang, children }) => {
     return (
-        <>
+        <div id="menu">
             <h2>{lang === "es" ? "He hecho..." : "I have done..."}</h2>
             <section className="menu-works">
                 <div className="options-menu">
-                    <a href="#projects">📁 {lang === "es" ? "Proyectos" : "Projects"}</a>
-                    <a href="#education">📒 {lang === "es" ? "Educación" : "Education"}</a>
-                    <a href="#books">🔖 {lang === "es" ? "Libros (leídos)" : "Books (read)"}</a>
-                    <a href="#now">📌 {lang === "es" ? "Actualmente" : "Now"}</a>
+                    {children}
                 </div>
             </section>
-        </>
+        </div>
     );
 }
 export default Menu;
